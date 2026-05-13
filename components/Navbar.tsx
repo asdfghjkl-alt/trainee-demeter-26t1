5df40720e4a1992c3c1c1926dfffdddf68c336ab
Edward Wenkai Liu <edward.liu2@student.unsw.edu.au>
2026-05-13 20:21:32 +1000

    Clean up Navbar imports and add loading skeletons for auth state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import Image from "next/image";
 import { ThemeToggle } from "./ThemeToggle";
 import { useAuth } from "@/contexts/AuthContext";
 import { useState, useRef, useEffect } from "react";
@@ -59,19 +58,24 @@ export default function Navbar() {
             {/* Left Side: Brand and Navigation Links */}
             <div className="flex items-center space-x-8">
               <Link href="/" className="flex items-center space-x-2 group">
-                <svg 
-                  className="w-7 h-7 text-cyan-600 dark:text-cyan-600 opacity-80 group-hover:opacity-100 transition-opacity" 
-                  fill="none" 
-                  xmlns="http://www.w3.org/2000/svg" 
+                <svg
+                  className="w-7 h-7 text-cyan-600 dark:text-cyan-600 opacity-80 group-hover:opacity-100 transition-opacity"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 16 16"
                   aria-label="Rendezvous Logo"
                 >
                   <g clipPath="url(#a)">
-                    <path fillRule="evenodd" clipRule="evenodd" d="M10.27 14.1a6.5 6.5 0 0 0 3.67-3.45q-1.24.21-2.7.34-.31 1.83-.97 3.1M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.48-1.52a7 7 0 0 1-.96 0H7.5a4 4 0 0 1-.84-1.32q-.38-.89-.63-2.08a40 40 0 0 0 3.92 0q-.25 1.2-.63 2.08a4 4 0 0 1-.84 1.31zm2.94-4.76q1.66-.15 2.95-.43a7 7 0 0 0 0-2.58q-1.3-.27-2.95-.43a18 18 0 0 1 0 3.44m-1.27-3.54a17 17 0 0 1 0 3.64 39 39 0 0 1-4.3 0 17 17 0 0 1 0-3.64 39 39 0 0 1 4.3 0m1.1-1.17q1.45.13 2.69.34a6.5 6.5 0 0 0-3.67-3.44q.65 1.26.98 3.1M8.48 1.5l.01.02q.41.37.84 1.31.38.89.63 2.08a40 40 0 0 0-3.92 0q.25-1.2.63-2.08a4 4 0 0 1 .85-1.32 7 7 0 0 1 .96 0m-2.75.4a6.5 6.5 0 0 0-3.67 3.44 29 29 0 0 1 2.7-.34q.31-1.83.97-3.1M4.58 6.28q-1.66.16-2.95.43a7 7 0 0 0 0 2.58q1.3.27 2.95.43a18 18 0 0 1 0-3.44m.17 4.71q-1.45-.12-2.69-.34a6.5 6.5 0 0 0 3.67 3.44q-.65-1.27-.98-3.1" fill="currentColor"/>
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M10.27 14.1a6.5 6.5 0 0 0 3.67-3.45q-1.24.21-2.7.34-.31 1.83-.97 3.1M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.48-1.52a7 7 0 0 1-.96 0H7.5a4 4 0 0 1-.84-1.32q-.38-.89-.63-2.08a40 40 0 0 0 3.92 0q-.25 1.2-.63 2.08a4 4 0 0 1-.84 1.31zm2.94-4.76q1.66-.15 2.95-.43a7 7 0 0 0 0-2.58q-1.3-.27-2.95-.43a18 18 0 0 1 0 3.44m-1.27-3.54a17 17 0 0 1 0 3.64 39 39 0 0 1-4.3 0 17 17 0 0 1 0-3.64 39 39 0 0 1 4.3 0m1.1-1.17q1.45.13 2.69.34a6.5 6.5 0 0 0-3.67-3.44q.65 1.26.98 3.1M8.48 1.5l.01.02q.41.37.84 1.31.38.89.63 2.08a40 40 0 0 0-3.92 0q.25-1.2.63-2.08a4 4 0 0 1 .85-1.32 7 7 0 0 1 .96 0m-2.75.4a6.5 6.5 0 0 0-3.67 3.44 29 29 0 0 1 2.7-.34q.31-1.83.97-3.1M4.58 6.28q-1.66.16-2.95.43a7 7 0 0 0 0 2.58q1.3.27 2.95.43a18 18 0 0 1 0-3.44m.17 4.71q-1.45-.12-2.69-.34a6.5 6.5 0 0 0 3.67 3.44q-.65-1.27-.98-3.1"
+                      fill="currentColor"
+                    />
                   </g>
                   <defs>
                     <clipPath id="a">
-                      <path fill="#fff" d="M0 0h16v16H0z"/>
+                      <path fill="#fff" d="M0 0h16v16H0z" />
                     </clipPath>
                   </defs>
                 </svg>
@@ -100,7 +104,12 @@ export default function Navbar() {
             <div className="flex items-center space-x-4 sm:space-x-6">
               <ThemeToggle />
 
-              {!isLoading && (
+              {isLoading ? (
+                <div className="flex items-center space-x-4 sm:space-x-6">
+                  <div className="hidden sm:block h-5 w-12 bg-gray-200 dark:bg-gray-800 rounded animate-pulse" />
+                  <div className="h-9 w-24 bg-gray-200 dark:bg-gray-800 rounded-full animate-pulse" />
+                </div>
+              ) : (
                 <>
                   {!user ? (
                     <>
