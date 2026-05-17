a57ac7990a38e1083fa02eb69686fe5ca4673042
Bryan Wen <z5691603@ad.unsw.edu.au>
2026-05-17 21:26:08 +1000

    Added the endpoint which tracks how many votes given in a room

---
+++ app/api/rooms/[code]/votes/status/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectToDatabase from "@/lib/mongodb";
+import { Vote, Room } from "@/database"
+import { apiHandler } from "@/lib/api-handler";
+import { getSession } from "@/lib/session";
+
+export const GET = apiHandler( async (
+    req: NextRequest,
+    { params }: { params: { code: String } }
+) => {
+    const roomCode = params.code;
+
+    await connectToDatabase();
+
+    const success = await Room.findOne({ code: roomCode});
+
+    // validate that the room exists
+    if (!success) {
+        return NextResponse.json(
+            { message: "Room not found" },
+            { status: 400 }
+        )
+    }
+
+    // obtain the number of votes with roomCode = room code
+    const numVotes = await Vote.countDocuments({ roomCode: roomCode});
+
+    return NextResponse.json(
+        { numVotes: numVotes },
+        { status: 200 }
+    )
+});
